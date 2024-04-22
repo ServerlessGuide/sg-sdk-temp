2af1d753598e08ea11a5a8cfa0654e67d050f37b
TaurusM <3075985899@qq.com>
2024-04-22 16:51:26 +0800

    then

--- sg-sdk-temp/src/lib.rs
+++ sg-sdk-temp/src/lib.rs
@@ -122,38 +122,38 @@ pub async fn init() {
     // 初始化业务码prefix，示例：
     // biz_code_prefix!(9801);
 
-    // 这些是固定的BizResult，范围：980000-999999，业务场景不能使用
+    // 这些是固定的BizResult，范围：0-999999，业务场景不能使用
     biz_result! {
-        (OK, 200, 980000, "success");
-        (URI_NOT_MATCH, 404, 980001, "uri match nothing");
-        (BODY_PARAMETER_ILLEGAL, 400, 980002, "body parameter illegal");
-        (CONVERT_TO_MODEL_ERROR, 500, 980003, "convert to model error");
-        (PARAMETER_ILLEGAL, 400, 980004, "parameter illegal");
-        (HEADER_NOT_FOUND, 400, 980005, "header not found");
-        (PARAM_MAP_PARSE_ERROR, 500, 980006, "param map parse error");
-        (PATH_PARAM_NOT_EXIST, 500, 980007, "path param not exist");
-        (BODY_PARAM_NOT_EXIST, 500, 980008, "body param not exist");
-        (QUERY_PARAM_NOT_EXIST, 500, 980009, "query param not exist");
-        (URL_PARSE_ERROR, 500, 980010, "url parse error");
-        (DAPR_HTTP_REQ_BUILD_ERROR, 500, 980011, "dapr request build error");
-        (DAPR_REQUEST_FAIL, 500, 980012, "dapr request fail");
-        (REQUEST_METHOD_NOT_ALLOWED, 500, 980013, "request method not allowed");
-        (ENV_PARAMETER_ERROR, 500, 980014, "env parameter error");
-        (DAPR_DATA_ILLEGAL, 500, 980015, "dapr data illegal");
-        (ENUM_NOT_FOUND, 500, 980016, "enum not found");
-        (IMPLICIT_RESPONSE_ERROR, 500, 980017, "implicit response error");
-        (BIZ_RESULT_NOT_FOUND, 500, 980018, "biz result not found");
-        (DAPR_CONFIG_NOT_EXIST, 500, 980019, "dapr config not exist");
-        (EXEC_NAME_NOT_EXIST, 500, 980020, "execute name not exist");
-        (DAPR_EXECUTE_NOT_EXIST, 500, 980021, "dapr execute not exist");
-        (QUERY_SQL_IS_NOT_UNIQUE, 500, 980022, "query sql is not unique");
-        (SQL_NOT_VALID, 500, 980023, "sql not valid");
-        (SQL_NOT_SUPPORT, 500, 980024, "sql not support");
-        (DATA_NOT_FOUND, 400, 980025, "data not found");
-        (SQL_OUT_COLUMNS_IS_EMPTY, 500, 980026, "sql out_columns is empty");
-        (DATA_ERROR, 500, 980027, "data error");
-        (AUTH_ERROR, 401, 980028, "auth error");
-        (INTERNAL_AUTH_TAG_NOT_SET, 500, 980029, "internal auth tag not set");
+        (OK, 200, 0, "success");
+        (URI_NOT_MATCH, 404, 11, "uri match nothing");
+        (BODY_PARAMETER_ILLEGAL, 400, 2, "body parameter illegal");
+        (CONVERT_TO_MODEL_ERROR, 500, 3, "convert to model error");
+        (PARAMETER_ILLEGAL, 400, 4, "parameter illegal");
+        (HEADER_NOT_FOUND, 400, 5, "header not found");
+        (PARAM_MAP_PARSE_ERROR, 500, 6, "param map parse error");
+        (PATH_PARAM_NOT_EXIST, 500, 7, "path param not exist");
+        (BODY_PARAM_NOT_EXIST, 500, 8, "body param not exist");
+        (QUERY_PARAM_NOT_EXIST, 500, 9, "query param not exist");
+        (URL_PARSE_ERROR, 500, 10, "url parse error");
+        (DAPR_HTTP_REQ_BUILD_ERROR, 500, 11, "dapr request build error");
+        (DAPR_REQUEST_FAIL, 500, 12, "dapr request fail");
+        (REQUEST_METHOD_NOT_ALLOWED, 500, 13, "request method not allowed");
+        (ENV_PARAMETER_ERROR, 500, 14, "env parameter error");
+        (DAPR_DATA_ILLEGAL, 500, 15, "dapr data illegal");
+        (ENUM_NOT_FOUND, 500, 16, "enum not found");
+        (IMPLICIT_RESPONSE_ERROR, 500, 17, "implicit response error");
+        (BIZ_RESULT_NOT_FOUND, 500, 18, "biz result not found");
+        (DAPR_CONFIG_NOT_EXIST, 500, 19, "dapr config not exist");
+        (EXEC_NAME_NOT_EXIST, 500, 20, "execute name not exist");
+        (DAPR_EXECUTE_NOT_EXIST, 500, 21, "dapr execute not exist");
+        (QUERY_SQL_IS_NOT_UNIQUE, 500, 22, "query sql is not unique");
+        (SQL_NOT_VALID, 500, 23, "sql not valid");
+        (SQL_NOT_SUPPORT, 500, 24, "sql not support");
+        (DATA_NOT_FOUND, 400, 25, "data not found");
+        (SQL_OUT_COLUMNS_IS_EMPTY, 500, 26, "sql out_columns is empty");
+        (DATA_ERROR, 500, 27, "data error");
+        (AUTH_ERROR, 401, 28, "auth error");
+        (INTERNAL_AUTH_TAG_NOT_SET, 500, 29, "internal auth tag not set");
     };
 
     // 初始化URI，示例：
